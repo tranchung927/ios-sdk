7ff5960c4931751b94ec41b99355c8b8c80d625c
ChungTV <chungtv@ftech.ai>
2022-05-11 17:18:55 +0700

    update mvvm sdk

---
+++ Sources/MVVM.xcframework/ios-arm64/MVVM.framework/Headers/MVVM.h
@@ -0,0 +1,18 @@
+//
+//  MVVM.h
+//  MVVM
+//
+//  Created by ChungTV on 11/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MVVM.
+FOUNDATION_EXPORT double MVVMVersionNumber;
+
+//! Project version string for MVVM.
+FOUNDATION_EXPORT const unsigned char MVVMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MVVM/PublicHeader.h>
+
+
