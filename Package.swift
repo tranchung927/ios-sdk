37ded623e9219b1e05752847f1972df4b5f8a8ea
ChungTV <chungtv@ftech.ai>
2022-05-11 17:23:31 +0700

    fix package

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MVVM",
+            name: "iOS-MVVM",
             targets: ["MVVM"]),
     ],
     dependencies: [
