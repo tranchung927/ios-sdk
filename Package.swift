6db619933de39dfa5537e8ccbf09008b3c04b581
ChungTV <chungtv@ftech.ai>
2022-05-11 17:43:20 +0700

    move frameworks

--- Package.swift
+++ Package.swift
@@ -11,7 +11,11 @@ let package = Package(
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "iOS SDK",targets: ["MVVM", "AppNetworking", "AppRouter", "CoordinatorLayout", "TransitionController"]),
+        .library(name: "MVVM",targets: ["MVVM"]),
+        .library(name: "AppNetworking",targets: ["AppNetworking"]),
+        .library(name: "AppRouter",targets: ["AppRouter"]),
+        .library(name: "CoordinatorLayout",targets: ["CoordinatorLayout"]),
+        .library(name: "TransitionController",targets: ["TransitionController"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
